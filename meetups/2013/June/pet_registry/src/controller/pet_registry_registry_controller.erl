a33aa5d6fe6f138fae1f6865fa0509ddaec7b0cc
Francis Joanis <francis.joanis@gmail.com>
2013-06-03 19:12:37 -0400

    Added some validation.
    Name must be unique, not be empty. Kind must not be empty. Still need to validate on Edit.

--- meetups/2013/June/pet_registry/src/controller/pet_registry_registry_controller.erl
+++ meetups/2013/June/pet_registry/src/controller/pet_registry_registry_controller.erl
@@ -15,9 +15,13 @@ add('GET', []) ->
 save('POST', []) ->
    Name = Req:post_param("Name"),
    Kind = Req:post_param("Kind"),
-   NewPet = pet:new(id, Name, Kind),
-   {ok, _} = NewPet:save(),
-   {redirect, [{action, "browse"}]};
+   case already_exists(Name) of
+      false ->
+         save_new(Name, Kind);
+      true ->
+         Errors = ["Name must not already exist"],
+         {render_other, [{action, "add"}], [{errors, Errors}, {name, Name}, {kind, Kind}]}
+   end;
 
 save('POST', [Id]) ->
    Name = Req:post_param("Name"),
@@ -30,3 +34,21 @@ save('POST', [Id]) ->
 delete('GET', [Id]) ->
    boss_db:delete(Id),
    {redirect, [{action, "browse"}]}.
+
+save_new(Name, Kind) ->
+   NewPet = pet:new(id, Name, Kind),
+   case NewPet:save() of
+      {ok, _} ->
+         {redirect, [{action, "browse"}]};
+      {error, Errors} ->
+         {render_other, [{action, "add"}], [{errors, Errors}, {name, Name}, {kind, Kind}]}
+   end.
+
+% TODO: Don't allow edit to change name to already existing pet
+% TODO: Generalize save_new so that it can also catch errors for edit
+
+already_exists(Name) ->
+   case boss_db:find(pet, [name, equals, Name]) of
+      [] -> false;
+      _ -> true
+   end.
--- meetups/2013/June/pet_registry/src/model/pet.erl
+++ meetups/2013/June/pet_registry/src/model/pet.erl
@@ -1,2 +1,19 @@
 -module(pet, [Id, Name, Kind]).
 -compile(export_all).
+
+validation_tests() ->
+   [
+      {
+         fun() ->
+            length(Name) > 0
+         end,
+         "Pet must have a name"
+      },
+      {
+         fun() ->
+            length(Kind) > 0
+         end,
+         "Pet must have a kind"
+      }
+
+   ].
