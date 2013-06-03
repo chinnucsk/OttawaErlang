d6e4f275fbeeaabd3cd72c3ab3972ad9b5e9c98a
Francis Joanis <francis.joanis@gmail.com>
2013-06-02 23:18:12 -0400

    Add/Edit/Delete now works

--- meetups/2013/June/pet_registry/src/controller/pet_registry_registry_controller.erl
+++ meetups/2013/June/pet_registry/src/controller/pet_registry_registry_controller.erl
@@ -6,14 +6,27 @@ browse('GET', []) ->
    {ok, [{pets, Pets}]}.
 
 edit('GET', [Id]) ->
-   {ok, []}.
+   Pet = boss_db:find(Id),
+   {ok, [{pet, Pet}]}.
 
 add('GET', []) ->
-   {ok, []};
+   {ok, []}.
 
-add('POST', []) ->
+save('POST', []) ->
    Name = Req:post_param("Name"),
    Kind = Req:post_param("Kind"),
    NewPet = pet:new(id, Name, Kind),
    {ok, _} = NewPet:save(),
+   {redirect, [{action, "browse"}]};
+
+save('POST', [Id]) ->
+   Name = Req:post_param("Name"),
+   Kind = Req:post_param("Kind"),
+   Pet = boss_db:find(Id),
+   NewPet = Pet:set([{name, Name}, {kind, Kind}]),
+   {ok, _} = NewPet:save(),
+   {redirect, [{action, "browse"}]}.
+
+delete('GET', [Id]) ->
+   boss_db:delete(Id),
    {redirect, [{action, "browse"}]}.
